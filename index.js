fae7a1fe8d48bd784a4ac20ae45edda433a50b1e
Paiz <Francopaiz100@gmail.com>
2024-07-13 00:49:56 -0300

    CORS agregados

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ config();
 const secretkey = process.env.SecretKey;
 const port = process.env.PORT || 3000; 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
